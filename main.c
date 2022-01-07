0bed87895822285554034ec68d02aed4a53f6b5f
dylou <dylan.innou@epita.fr>
2022-01-07 16:21:45 +0100

    [+] main function

--- main.c
+++ main.c
@@ -1,17 +1,38 @@
 #include <err.h>
 #include <errno.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv ){
-    if(argc < 2)
-    {
-        errx(1,"error");
-    }
-    if(!argv[1])
-    {
-        return 1;
+#define BUFF_SIZE 256
+
+int my_strcmp(char* str1, char* str2){
+    for (;*str1 != '\0' && *str2 != '\0'; str1++ , str2++)
+        if (*str1 != *str2)
+            return 0;
+    return (*str1 == '\0' && *str2 == '\0');
+}
+
+void xwrite(char *str, int size){
+    ssize_t output = write(1, str, size);
+    if (output == -1)
+        errx(1, "error in buffer");
+}
+
+void xread(char *buff, int size){
+    ssize_t output = read(0, buff, size);
+    if (output == -1)
+        errx(1, "error in buffer");
+}
+
+int main(void){//int argc, char **argv ){
+    char *buff = calloc(BUFF_SIZE, sizeof(char));
+    *buff = '\0';
+    while (my_strcmp(buff, "quit\n") == 0){
+        xwrite("> ", 2);
+        xread(buff, BUFF_SIZE);
+        xwrite("", 1);
     }
     return 0;
 }
